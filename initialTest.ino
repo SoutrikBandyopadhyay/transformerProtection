b3732bdda9569b79a72f075c5231d26e60aa0a9b
Soutrik Bandyopadhyay <soutrik.band@gmail.com>
2017-04-09 17:15:34 +0530

    Adding Pins of LCD Display
    Nokia 5110

--- initialTest.ino
+++ initialTest.ino
@@ -1,9 +1,26 @@
+//Nokia 5110 Display Variables
+
+#define RST 12
+#define CE 11
+#define DC 10
+#define DIN 9
+#define CLK 8
+
+// LCD Writer
+
+void lcdWrite(byte cmd){
+  digitalWrite(DC,LOW);
+  digitalWrite(CE,LOW);
+  shiftOut(DIN,CLK,MSBFIRST,cmd);
+  digitalWrite(CE,HIGH);
+}
+
+
+
 void setup() {
-  // put your setup code here, to run once:
       
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
